7cba8c6975f6114bfbc7e746f590c692584c9add
이주영 <wruomma2@naver.com>
2024-02-07 22:49:21 +0900

    :tada: tada: 프로젝트 생성

---
+++ src/test/java/com/jy/myblog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
