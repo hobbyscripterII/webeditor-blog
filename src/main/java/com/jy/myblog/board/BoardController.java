23b8f6e50e8d07cd5a520351e400368e1a8d549e
hobbyscripterII <wruomma2@naver.com>
2024-02-08 17:06:38 +0900

    :lipstick: style: UI 수정

--- src/main/java/com/jy/myblog/board/BoardController.java
+++ src/main/java/com/jy/myblog/board/BoardController.java
@@ -3,11 +3,18 @@ package com.jy.myblog.board;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/board")
 public class BoardController {
     private final BoardService service;
 
+    @GetMapping("/list")
+    public String board() {
+        return "/board/list";
+    }
 }
